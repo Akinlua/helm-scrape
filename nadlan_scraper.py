ccafffdd0da7463596cc0358cb3c11ab4e6dd8ae
Tesals <akinluaolorunfunminiyi@gmail.com>
2025-01-31 03:09:57 +0100

    fits

--- nadlan_scraper.py
+++ nadlan_scraper.py
@@ -25,36 +25,38 @@ def setup_driver():
     options.add_argument("window-size=1920x1080")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument('--enable-unsafe-swiftshader')
+    options.add_argument('--ignore-gpu-blocklist')
     
-    # # Disable unnecessary features
-    # prefs = {
-    #     'profile.default_content_setting_values': {
-    #         'images': 2,
-    #         'plugins': 2,
-    #         'popups': 2,
-    #         'geolocation': 2,
-    #         'notifications': 2,
-    #         'auto_select_certificate': 2,
-    #         'fullscreen': 2,
-    #         'mouselock': 2,
-    #         'mixed_script': 2,
-    #         'media_stream': 2,
-    #         'media_stream_mic': 2,
-    #         'media_stream_camera': 2,
-    #         'protocol_handlers': 2,
-    #         'ppapi_broker': 2,
-    #         'automatic_downloads': 2,
-    #         'midi_sysex': 2,
-    #         'push_messaging': 2,
-    #         'ssl_cert_decisions': 2,
-    #         'metro_switch_to_desktop': 2,
-    #         'protected_media_identifier': 2,
-    #         'app_banner': 2,
-    #         'site_engagement': 2,
-    #         'durable_storage': 2
-    #     }
-    # }
-    # options.add_experimental_option('prefs', prefs)
+    # Disable unnecessary features
+    prefs = {
+        'profile.default_content_setting_values': {
+            'images': 2,
+            'plugins': 2,
+            'popups': 2,
+            'geolocation': 2,
+            'notifications': 2,
+            'auto_select_certificate': 2,
+            'fullscreen': 2,
+            'mouselock': 2,
+            'mixed_script': 2,
+            'media_stream': 2,
+            'media_stream_mic': 2,
+            'media_stream_camera': 2,
+            'protocol_handlers': 2,
+            'ppapi_broker': 2,
+            'automatic_downloads': 2,
+            'midi_sysex': 2,
+            'push_messaging': 2,
+            'ssl_cert_decisions': 2,
+            'metro_switch_to_desktop': 2,
+            'protected_media_identifier': 2,
+            'app_banner': 2,
+            'site_engagement': 2,
+            'durable_storage': 2
+        }
+    }
+    options.add_experimental_option('prefs', prefs)
     
     service = Service(log_output=os.devnull)
     return webdriver.Chrome(options=options, service=service)
